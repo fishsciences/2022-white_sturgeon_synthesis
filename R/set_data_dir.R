b2923bb9a48c7b1a336f7a0ce0dd92ab6fb26278
Myfanwy <myfanwy.johnston@fishsciences.net>
2022-04-26 14:55:37 -0700

    restructure how data is called

---
+++ R/set_data_dir.R
@@ -0,0 +1,15 @@
+# Sets the data directory once and then looks up
+# M. Espe, M. Johnston
+
+data_dir =
+  switch(Sys.info()["user"],
+         myfanwyjohnston = "~/DropboxCFS/NEW PROJECTS - EXTERNAL SHARE/WST_Synthesis/Data",
+         matt = "/home/matt/consulting/cfs/Projects/WST_Synthesis/Data/",
+         stop("User not known")
+  )
+
+# Make sure the directory is actually there
+if(!dir.exists(data_dir))
+  stop("data directory does not exist on this system. Please check R/set_data_dir.R")
+
+saveRDS(data_dir, "data/data_dir_local.rds")
