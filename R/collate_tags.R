74b92264ce000d661ecec7813111f1749539d3b5
Myfanwy <myfanwy.johnston@fishsciences.net>
2022-08-11 17:49:39 -0700

    clean up collate tags script

--- R/collate_tags.R
+++ R/collate_tags.R
@@ -3,82 +3,107 @@
 # Wed Apr 20 13:48:56 2022 ------------------------------
 source('R/utils.R') # sources data-dir script to set dropbox path
 # columns needed:
-#Tag_ID, Tag_ser_num, Codespace, Tag_type, Frequency_kHz, Release_date_time, Release_location, Study_ID
+#Tag_ID, DateTagged, Codespace, Freq_kHz, Tagging/Release_location, StudyID, 
+# FL_cm, TagLife_days, TagEnd, Sex
 
+# ONly want V16 tags - no V13s or PIT tags
+# All SJR and SAC fish are 10-year tags; YOLO you have to use the TAgLifeEnd column
+
+library(lubridate)
+keepcols = c("StudyID", 
+             "DateTagged",
+             "TagID",
+             "CodeSpace",
+             "TagCode",
+             "Release_location",
+             "FL_cm",
+             "Sex",
+             "TagEnd")
+# ------------------------------------
+# San Joaquin River Tags
+#-------------------------------------
 d = readxl::read_excel(file.path(data_dir, "Lodi/LFWO_SJR_WST_Acoustic_Tags.xlsx"))
 
-d = d[ , c("TagCode", "SN", "DateTagged", "FL_cm", "PIT")]
+d = d[ , c("TagCode", "DateTagged", "FL_cm", "Sex")]
 
-d = ybt::parse_tagid_col(df = d, tagcol = "TagCode", sepchar = "-")
-str(d)
+d = tidyr::separate(d, TagCode, into = c("Freq_kHz", "CodeSpace", "TagID"), remove = FALSE, convert = TRUE)
 
-d$Frequency_kHz = 69
 # no data on time of tagging/release; default to 8am PST on the date tagged:
-d$Release_date_time = lubridate::ymd_hms(paste(d$DateTagged, "08:00:00", sep = " "), tz = "Etc/GMT+8")
+d$DateTagged = as.Date(d$DateTagged)
 d$Release_location = "San Joaquin River"
-d$Study_ID = "SJR WST/Jackson/Heironimus"
-d = dplyr::rename(d, Tag_ser_num = SN)
-
+d$StudyID = "SJR WST"
 range(d$DateTagged)
 
-write.csv(d, "data_clean/JacksonHeironimusSJRWST_NEWTAGS.csv", row.names = FALSE)
+d$TagEnd = as.POSIXct((d$DateTagged + 3650), format = "%Y-%m-%d")
 
-y = readxl::read_excel(file.path(data_dir, "Yolo/wst_tags.xlsx"))
-y = y[ , c("DateTagged", "FL", "CodeSpace", "TagID", "TagSN")]
-y = dplyr::rename(y, FL_cm = FL,
-                     Tag_ser_num = TagSN)
+stopifnot(all(keepcols %in% colnames(d)))
 
-y$Frequency_kHz = 69
-y$Release_location = "Yolo Bypass"
-y$Study_ID = "UCD Yolo Bypass WST/Johnston"
-y$DateTagged = as.Date(y$DateTagged)
-range(y$DateTagged)
+d = d[ , keepcols]
 
+# ------------------------------------
+# Yolo Tags
+#-------------------------------------
+y = readxl::read_excel(file.path(data_dir, "Yolo/wst_all_metadata.xlsx"))
+y2 = readxl::read_excel(file.path(data_dir, "Yolo/wst_tags.xlsx"))
+
+y2 = y2[ , c("DateTagged", "CodeSpace", "TagID", "Sex")]
+y2$TagCode = paste("A69-", y2$CodeSpace, "-", y2$TagID, sep = "")
+y2 = merge(y2, y[ , c( "Tag ID Number", "Fork Length (cm)", "Tag Life End")], by.x = "TagID", by.y = "Tag ID Number")
+
+y2$DateTagged = as.Date(y2$DateTagged)
+
+y2 = dplyr::rename(y2, FL_cm = `Fork Length (cm)`,
+                     TagEnd = `Tag Life End`)
+
+y2$Freq_kHz = "A69"
+y2$Release_location = "Yolo Bypass"
+y2$StudyID = "YOLO WST"
+y2[ , c("TagID", "CodeSpace")] <- lapply(y2[ , c("TagID", "CodeSpace")], as.integer)
+
+stopifnot(all(keepcols %in% colnames(y2)))
+
+y2 = y2[ , keepcols]
+
+# ------------------------------------
+# Sacramento River Tags
+#-------------------------------------
 m = read.csv(file.path(data_dir, "Sacramento/Miller_USACE_white_sturgeon_tag_ids.csv"))
-str(m)
+
 m = m[, c(
   "Date",
   "Location",
   "Fork.length.cm.",
+  "Sex",
   "Tag.ID.",
-  "Tag.Sn.",
-  "Pit.Tag.ID",
-  "Tag.Type",
-  "Time.of.Capture"
+  "Tag.Type"
 )]
+# get rid of v13s
+m = m[m$Tag.Type != "v13" & !is.na(m$`Tag.ID.`), ]
 
 m = dplyr::rename(m, 
                   FL_cm = Fork.length.cm., 
                   DateTagged = Date,
                   Release_location = Location,
-                  TagID = Tag.ID.,
-                  Tag_ser_num = Tag.Sn.,
-                  PIT = Pit.Tag.ID)
+                  TagID = Tag.ID.)
 
 m$DateTagged = as.Date(m$Date, format = "%m/%d/%y")
 
-m = m[m$Tag.Type != "v13" & !is.na(m$TagID), ]
+m$Freq_kHz = "A69"
+m$TagCode = paste(m$Freq_kHz, m$CodeSpace, m$TagID, sep = "-")
+m$StudyID = "SAC WST"
+m$CodeSpace = 1303L
 
-m$Time.of.Capture[is.na(m$Time.of.Capture)] <- "08:00" # assign NAs to 8am on DateTagged
-m$Time.of.Capture[m$Time.of.Capture == ""] <- "08:00" # assign NAs to 8am on DateTagged
+m$TagEnd = as.POSIXct((m$DateTagged + 3650), format = "%Y-%m-%d")
 
-m$Release_date_time = lubridate::ymd_hm(paste(as.character(m$DateTagged), 
-                                             m$Time.of.Capture, sep = " "), tz = "Etc/GMT+8")
+m = dplyr::select(m, TagID, DateTagged, Release_location, FL_cm, Sex, StudyID, CodeSpace, TagCode, TagEnd)
 
-m$Study_ID = "UCD Sacramento WST/Miller"
-m$CodeSpace = 1303
-m = dplyr::select(m, TagID, Tag_ser_num, PIT, DateTagged, Release_location, FL_cm, Study_ID, CodeSpace)
-m$Frequency_kHz = 69
+stopifnot(all(keepcols %in% colnames(m)))
 
-x = merge(m, y, all = TRUE)
-colSums(is.na(x))
+m = m[ , keepcols]
 
-write.csv(x, "data_clean/MillerJohnston_WSTUCD_OLDTAGS.csv", row.names = FALSE)
-
-str(x)
-str(d)
-all.tags = merge(x, d, all = TRUE)
+# all = merge x and San Joaquin River tags
+all.tags = dplyr::bind_rows(d, y2, m)
 csn(all.tags)
-
-dput(all.tags$TagID)
 range(all.tags$DateTagged)
+
+saveRDS(all.tags, "data_clean/alltags.rds")
